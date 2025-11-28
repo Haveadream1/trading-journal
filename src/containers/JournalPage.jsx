a64625b40654dcca30b9e9ed4757c5ae9e0db6b9
Haveadream1 <260602david@gmail.com>
2025-11-28 10:21:26 +0100

    feat: convert html journal page into a react component

---
+++ src/containers/JournalPage.jsx
@@ -0,0 +1,52 @@
+export default function JournalPage() {
+    return (
+        <main className="journal-page">
+            <section className="main-heading-section" aria-labelledby="main-heading">
+                <h1 id="main-heading">Trades journal</h1>
+                <p className="main-heading-text">A complete history of your recent trades</p>
+            </section>
+
+            <table>
+                <thead>
+                    <tr className="table-heading-row">
+                        <th>Date</th>
+                        <th>Asset</th>
+                        <th>Direction</th>
+                        <th>Outcome</th>
+                        <th>Net P/L</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr className="table-values-row">
+                        <td className="table-date-value">24/05/2025</td>
+                        <td className="table-asset-value">BTC / USD</td>
+                        <td className="table-direction-value">BUY</td>
+                        <td className="table-outcome-value">
+                            <span className="outcome-span-win">Win</span>
+                        </td>
+                        <td className="table-pnl-value">
+                            <span className="pnl-span-win">+2,505.20$</span>
+                        </td>
+                    </tr>
+
+                    <tr className="table-values-row">
+                        <td className="table-date-value">25/05/2025</td>
+                        <td className="table-asset-value">ETH / USD</td>
+                        <td className="table-direction-value">SELL</td>
+                        <td className="table-outcome-value">
+                            <span className="outcome-span-loss">Loss</span>
+                        </td>
+                        <td className="table-pnl-value">
+                            <span className="pnl-span-loss">-1,204.05$</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button type="button" className="add-new-trade-button">
+                <span className="material-symbols-outlined" alt="Add a new trade icon" aria-label="Add a new trade" loading="lazy">add_2</span>
+            </button>
+        </main>
+    );
+}
