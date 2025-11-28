b0319e5294805ab1472e87763d7a8cd0cead6774
Haveadream1 <260602david@gmail.com>
2025-11-28 20:20:02 +0100

    feat: add router to handle the website navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './components/Header'
+import DashboardPage from './containers/DashboardPage'
+import JournalPage from './containers/JournalPage'
+import TradeFormPage from './containers/TradeFormPage'
+import AnalyticsPage from './containers/AnalyticsPage'
+import { RouterProvider } from 'react-router'
+import { createBrowserRouter } from 'react-router'
+
+
+// Router will take care of the webiste navigation
+// TODO add errorElement
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <DashboardPage />,
+  },
+  {
+    path:"/journal",
+    element: <JournalPage />,
+  },
+  {
+    path:"/analytics",
+    element: <AnalyticsPage />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} /> 
+    {/* // we pass the defined path above as a value */}
   </StrictMode>,
 )
