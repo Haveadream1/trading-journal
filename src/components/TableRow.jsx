a1274ac2e13eec75324aed96db4c2b058c346291
Haveadream1 <260602david@gmail.com>
2025-11-28 14:36:51 +0100

    feat: create a table row component

---
+++ src/components/TableRow.jsx
@@ -0,0 +1,29 @@
+export default function TableRow({
+    date,
+    asset,
+    direction,
+    outcome,
+    pnl
+}) {
+    return (
+        <tr className="table-values-row">
+            <td className="table-date-value">{date}</td>
+            <td className="table-asset-value">{asset}</td>
+            <td className="table-direction-value">{direction}</td>
+            <td className="table-outcome-value">
+                {outcome === "Loss" ? (
+                    <span className="outcome-span-loss">Loss</span>
+                ):(
+                    <span className="outcome-span-win">Win</span>
+                )}
+            </td>
+            <td className="table-pnl-value">
+                {outcome === "Loss" ? (
+                    <span className="pnl-span-loss">{pnl}</span>
+                ):(
+                    <span className="pnl-span-win">{pnl}</span>
+                )}
+            </td>
+        </tr>
+    );
+}
