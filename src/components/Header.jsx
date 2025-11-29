ac1e7f0ebb3a38c4ec3ad7842fd617227f18f9fd
Haveadream1 <260602david@gmail.com>
2025-11-29 08:55:50 +0100

    feat: target the current displayed page with Router Link and useLocation hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
+import { Link, useLocation } from "react-router";
+
 export default function Header({ }) {
+    const location = useLocation();
+    const path = location.pathname;
+    // With the path fetched of the current displayed page thanks to the router
+    // We can render conditionally if the page is the one currently rendered
+
     return (
         <header role="banner">
             <div className="icon-title-container">
@@ -7,16 +14,29 @@ export default function Header({ }) {
             </div>
 
             <nav aria-label="Website navigation">
-                <a href="/" aria-current="page" className="nav-link nav-link-active">Dashboard</a>
-                <a href="/journal" className="nav-link">Journal</a>
-                <a href="/analytics" className="nav-link">Analytics</a>
+                <Link 
+                    to="/" 
+                    aria-current={path === '/' && 'page'}
+                    className="nav-link">
+                    Dashboard
+                </Link>
+                <Link
+                    to="/journal"
+                    aria-current={path === '/journal' && 'page'}
+                    className="nav-link">
+                    Journal
+                </Link>
+                <Link 
+                    to="/analytics" 
+                    aria-current={path === '/analytics' && 'page'}
+                    className="nav-link">
+                    Analytics
+                </Link>
             </nav>
 
             <div className="profile-container">
-                <a href="/profile" className="profile-link" aria-label="View user profile"></a>
+                <Link to="/profile" className="profile-link" aria-label="View user profile"></Link>
             </div>
         </header>
-    )
+    );
 }
-
-{/* <img class="profile-picture" src="" alt="User profile picture" loading="lazy"></img> */}
