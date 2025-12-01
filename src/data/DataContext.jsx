b309326f160d66ea30fa92dc055ec4bf577a8d0a
Haveadream1 <260602david@gmail.com>
2025-12-01 15:54:01 +0100

    feat: init default data context structure

--- src/data/DataContext.jsx
+++ src/data/DataContext.jsx
@@ -1,6 +1,26 @@
 
 // Fetch the data from the *Financial Modeling Prep API*
 // Only need one call and we can they use the data
-
 // The current goal is to fetch a list of 50 most commons assets for our dataList
 
+
+import { createContext, useContext } from "react";
+
+const DataContext = createContext();
+
+export function DataProvider({ children }) {
+    
+    return (
+        <DataContext.Provider value={value}>
+            {children}
+        </DataContext.Provider>
+    );
+}
+
+export const useData = () => {
+    const context = useContext(DataContext);
+    if (!context) {
+        throw new Error("useData must be used inside a DataProvider");
+    }
+    return context;
+}
