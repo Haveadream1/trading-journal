45a43961a27a7cefb79f873211d2e7e9d442d5d6
Haveadream1 <260602david@gmail.com>
2025-12-01 15:49:37 +0100

    feat: init DataContext file to fetch data

---
+++ src/data/DataContext.jsx
@@ -0,0 +1,6 @@
+
+// Fetch the data from the *Financial Modeling Prep API*
+// Only need one call and we can they use the data
+
+// The current goal is to fetch a list of 50 most commons assets for our dataList
+
