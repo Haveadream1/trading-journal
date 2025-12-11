3a977f27811c4bd0d9da6c51e7611c5bb90b4241
Haveadream1 <260602david@gmail.com>
2025-12-11 17:26:12 +0100

    feat: add conditional rendering for text

--- src/components/AnalyticSection.jsx
+++ src/components/AnalyticSection.jsx
@@ -1,13 +1,19 @@
 export default function AnalyticSection({
     titleId,
     titleValue,
-    alt
+    src,
+    alt,
+    text
 }) {
     return (
         <section className="analytic-section" aria-labelledby={titleId}>
             <h2 id={titleId}>{titleValue}</h2>
-            <img src="" alt={alt} loading="lazy" />
+            {src ? (
+                <img src="" alt={alt} loading="lazy" />
+            ):(
+                <p>{text}</p>
+            )}
         </section>
     );
 }
-// TODO Pass image source
+// TODO: Either we pass the graph in the analytic section Either we call it in the parent -> Wait for real data implementation
