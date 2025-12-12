bfbb217eb0927da132ed256b3b60f25f6e64a2c4
Haveadream1 <260602david@gmail.com>
2025-12-12 09:33:18 +0100

    refactor: import SideMetric component

--- src/containers/DashboardPage.jsx
+++ src/containers/DashboardPage.jsx
@@ -1,18 +1,9 @@
 import MainHeading from "../components/MainHeading";
 import MetricSection from "../components/MetricSection";
 import Header from "../components/Header";
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import SideMetricSection from "../components/SideMetricSection";
 
 export default function DashboardPage({ }) {
-    const trades = [
-        { id: 1, asset: 'AAPL', direction: 'Buy', outcome: 'Win', date: '2024-01-15', netPNL: 200},
-        { id: 2, asset: 'NVDA', direction: 'Sell', outcome: 'Loss', date: '2024-01-16', netPNL: -100},
-        { id: 3, asset: 'AUD/USD', direction: 'Buy', outcome: 'Win', date: '2024-01-17', netPNL: 150},
-        { id: 4, asset: 'ETH', direction: 'Sell', outcome: 'Win', date: '2024-01-18', netPNL: 350},
-        { id: 5, asset: 'BTC', direction: 'Buy', outcome: 'Loss', date: '2024-01-19', netPNL: -200},
-        { id: 6, asset: 'USD coin', direction: 'Sell', outcome: 'Loss', date: '2024-01-20', netPNL: -150},
-    ];
-
     return (
         <>
             <Header />
@@ -43,28 +34,23 @@ export default function DashboardPage({ }) {
                 </div>
 
                 <div className="graphic-metrics-grid">
-                    <ResponsiveContainer className="trade-results-graphic"> {/* Allow to fix issue of page resizing with this recharts import */}
-                        <AreaChart responsive data={trades}>
-                            <XAxis dataKey="date" />
-                            <YAxis width="auto" type="number" />
-                            <Tooltip />
-                            <Area type="monotone" dataKey="netPNL" stroke="#11D473" fill="#11D473" />
-                        </AreaChart>
-                    </ResponsiveContainer>
+                    <SideMetricSection 
+                        titleId="metric-heading-graph" 
+                        titleValue="Trades graphic" 
+                        graphMetric="true" 
+                    />
 
-                    <section className="side-metric-section" aria-labelledby="side-metric-heading-asset">
-                        <h3 id="side-metric-heading-asset">Most traded asset</h3>
-                        <p className="asset-value">XAU / USD</p>
-                        <p className="trade-count">48 Trades</p>
-                    </section>
+                    <SideMetricSection 
+                        titleId="side-metric-heading-asset" 
+                        titleValue="Most traded asset" 
+                        assetMetric="true" 
+                    />
 
-                    <section className="side-metric-section" aria-labelledby="side-metric-heading-economic-event">
-                        <h3 id="side-metric-heading-economic-event">Upcoming economic red events:</h3>
-                        <ul className="economic-event-list">
-                            <li className="economic-event-value">Core PPI / 2  PM / USD</li>
-                            <li className="economic-event-value">Retail sales / 2: 30PM / CHF</li>
-                        </ul>
-                    </section>
+                    <SideMetricSection 
+                        titleId="side-metric-heading-economic-even" 
+                        titleValue="Upcoming economic red events:" 
+                        economicMetric="true" 
+                    />
                 </div>
             </main>
         </>
