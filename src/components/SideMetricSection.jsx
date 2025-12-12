7b7653cf3a85af6bf9c2d6aa9c27fe02823a7a3e
Haveadream1 <260602david@gmail.com>
2025-12-12 09:33:53 +0100

    feat: create component to handle side metric with conditional render

---
+++ src/components/SideMetricSection.jsx
@@ -0,0 +1,46 @@
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+export default function SideMetricSection({ 
+    titleId,
+    titleValue,
+    graphMetric,
+    assetMetric,
+    economicMetric
+}) {
+    const trades = [
+        { id: 1, asset: 'AAPL', direction: 'Buy', outcome: 'Win', date: '2024-01-15', netPNL: 200},
+        { id: 2, asset: 'NVDA', direction: 'Sell', outcome: 'Loss', date: '2024-01-16', netPNL: -100},
+        { id: 3, asset: 'AUD/USD', direction: 'Buy', outcome: 'Win', date: '2024-01-17', netPNL: 150},
+        { id: 4, asset: 'ETH', direction: 'Sell', outcome: 'Win', date: '2024-01-18', netPNL: 350},
+        { id: 5, asset: 'BTC', direction: 'Buy', outcome: 'Loss', date: '2024-01-19', netPNL: -200},
+        { id: 6, asset: 'USD coin', direction: 'Sell', outcome: 'Loss', date: '2024-01-20', netPNL: -150},
+    ];
+
+    return (
+        <section className={graphMetric ? ("trade-results-graphic") : ("side-metric-section")} aria-labelledby={titleId}>
+            <h3 id={titleId}>{titleValue}</h3>
+            {graphMetric && (
+                <ResponsiveContainer> {/* Allow to fix issue of page resizing with this recharts import */}
+                    <AreaChart responsive data={trades}>
+                        <XAxis dataKey="date" />
+                        <YAxis width="auto" type="number" />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="netPNL" stroke="#11D473" fill="#11D473" />
+                    </AreaChart>
+                </ResponsiveContainer>
+            )}
+            {assetMetric && (
+                <>
+                    <p className="asset-value">XAU / USD</p>
+                    <p className="trade-count">48 Trades</p>
+                </>
+            )}
+            {economicMetric && (
+                <ul className="economic-event-list">
+                    <li className="economic-event-value">Core PPI / 2  PM / USD</li>
+                    <li className="economic-event-value">Retail sales / 2: 30PM / CHF</li>
+                </ul>
+            )}
+        </section>
+    );
+}
