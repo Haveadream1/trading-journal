683673b2e875833f6a281d103ead6b9f303da9bd
Haveadream1 <260602david@gmail.com>
2025-12-13 11:48:54 +0100

    feat: import data from API key into our dataContext

--- src/data/DataContext.jsx
+++ src/data/DataContext.jsx
@@ -1,17 +1,34 @@
-
 // Fetch the data from the *Financial Modeling Prep API*
 // Only need one call and we can they use the data
-// The current goal is to fetch a list of 50 most commons assets for our dataList
+// The current goal is to fetch a list of recent news for our dataList
 
-
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DataContext = createContext();
 
 export function DataProvider({ children }) {
+    const [articles, setArticles] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // ! Do not push API key to github
+                const response = await fetch("");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data", error);
+                }
+
+                const result = await response.json();
+                setArticles(result); // Pass the fetched data in the state
+            } catch (error) {
+                throw new Error("An error was catched while fetching data", error);
+            }
+        }
+        fetchData();
+    }, [])
     
     return (
-        <DataContext.Provider value={value}>
+        <DataContext.Provider value={articles}>
             {children}
         </DataContext.Provider>
     );
