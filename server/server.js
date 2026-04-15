695faf0d92c6e8ae090f472f74bd26a53c90bbd6
Haveadream1 <260602david@gmail.com>
2026-04-15 17:48:15 +0900

    feat: add middlewares

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-// start the web server with the port 3000
-const PORT = 3000;
+// Fetch from env file or provide default port
+const PORT = process.env.PORT || 3000;
+
+// start the web server with the port from env
 app.listen(PORT, () => {
   console.log(`Server is UP !`);
 });
 
-// Set up middleware
+// Set up middleware, act as a bridge for communication between Frontend and Backend
+app.use(cors()); // Enable React to communicate with the server
+app.use(express.json());  // Enable the server to parse(read) JSON data from React
+
 // Create connection with database from env URL
 // Set up routes
