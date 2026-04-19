dcc05c46a83cadb57f5b8198e329e59ef51e5ab2
Haveadream1 <260602david@gmail.com>
2026-04-19 15:16:03 +0900

    feat: setup a test route

--- server/server.js
+++ server/server.js
@@ -5,16 +5,28 @@ const cors = require('cors');
 const app = express();
 const pool = require('./database'); // Import the database connection pool
 
+// Fetch from env file or provide default port
+const PORT = process.env.PORT || 3000;
+
 // Set up middleware, act as a bridge for communication between Frontend and Backend
 app.use(cors()); // Enable React to communicate with the server
 app.use(express.json());  // Enable the server to parse(read) JSON data from React
 
-// Fetch from env file or provide default port
-const PORT = process.env.PORT || 3000;
+// Set up test route
+  // api/health is a common used route to check if server is running
+app.get('/api/health', (req, res) => {
+  // Adding the formatted timestamp is a good practice for debugging
+  res.json({ 
+    status: 'Server is running',
+    timestamp: new Date().toLocaleString() // ie: 4/19/2026, 3:09:40 PM
+  });
+});
 
-// start the web server with the port from env
+// Start the server with the port from env
+  // Must be always at the end of the file
 app.listen(PORT, () => {
-  console.log(`Server is UP !`);
+  console.log(`Server is UP on http://localhost:${PORT}`);
+  console.log(`Test route is UP on http://localhost:${PORT}/api/health`);
 });
 
 // Create connection with database from env URL
