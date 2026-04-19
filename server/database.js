4cef7e9109c662f5eed21441efd5f9ab88ca8e6f
Haveadream1 <260602david@gmail.com>
2026-04-19 14:38:47 +0900

    feat: add database connection test

--- server/database.js
+++ server/database.js
@@ -1,9 +1,25 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
-// Create a database connection pool with the databaseURL from Neon
+// Create a database connection pool
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    // Neon connection URL from env file
+    connectionString: process.env.DATABASE_URL,
+
+    // Security protocol that encrypts the connection between the server and the database
+    ssl: {
+        // SSL is normally required, but for this university project we can avoid complex setup
+        rejectUnauthorized: false
+    }
+});
+
+// Connection test
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Connection to database failed', err.message);
+    } else {
+        console.log('Connection to database successful');
+        release(); // Important function to go back to the pool 
+    }
 });
 
 module.exports = pool;
