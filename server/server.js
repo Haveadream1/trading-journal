b12c488c69c58a5defaf57e1c2adec89b82b9271
Haveadream1 <260602david@gmail.com>
2026-04-22 11:40:31 +0900

    feat: set up basic route to get all trades

--- server/server.js
+++ server/server.js
@@ -22,10 +22,22 @@ app.get('/api/health', (req, res) => {
   });
 });
 
+// Set up route to get all trades
+  // Use asyncronous function to handle the database query
+app.get('/api/trades', async (req, res) => {
+  try {
+    const allTrades = await pool.query('SELECT * FROM trades'); // PostgreSQL query statement 
+    res.json(allTrades.rows); // Express method to send a JSON response to the client
+  } catch (err) {
+    console.error('Error fetching all trades from database', err.message); // Log error for debugging
+    res.status(500).json({ error: 'Error fetching all trades from database' }); // Send error to the client (500: server error)
+  }
+});
+
 // Start the server with the port from env
   // Must be always at the end of the file
 app.listen(PORT, () => {
-  console.log(`Server is UP on http://localhost:${PORT}`);
+  console.log(`Server is UP on http://localhost:${PORT}/api/trades`);
   console.log(`Test route is UP on http://localhost:${PORT}/api/health`);
 });
 
