2d90facaa6c4d7f34682a573250474615359da31
Haveadream1 <260602david@gmail.com>
2026-04-22 16:56:31 +0900

    feat; setup POST route

--- server/server.js
+++ server/server.js
@@ -22,15 +22,46 @@ app.get('/api/health', (req, res) => {
   });
 });
 
-// Set up route to get all trades
+// Set up GET route to get all trades
   // Use asyncronous function to handle the database query
 app.get('/api/trades', async (req, res) => {
   try {
     const allTrades = await pool.query('SELECT * FROM trades'); // PostgreSQL query statement 
     res.json(allTrades.rows); // Express method to send a JSON response to the client
   } catch (err) {
-    console.error('Error fetching all trades from database', err.message); // Log error for debugging
-    res.status(500).json({ error: 'Error fetching all trades from database' }); // Send error to the client (500: server error)
+    console.error('Error reading all trades from database', err.message); // Log error for debugging
+    res.status(500).json({ error: 'Error reading all trades from database' }); // Send error to the client (500: server error)
+  }
+});
+
+// Set up POST route
+app.post('/api/trades', async (req, res) => {
+  try {
+    const {trade_date, asset, direction, outcome, net_pnl} = req.body; // Destructure trade data from the client request 
+
+    // Check if data is valid
+    if (!trade_date || !asset || !direction || !outcome || net_pnl === null) { // net_pnl can have the 0 value, so check instead for undefined and null values
+      // 400: client sent an invalid request
+      return res.status(400).json({ error: 'Error with required inputs' });
+    }
+
+    // Prevent from SQL injection with parameterized query
+      // Parmeterized separate the SQL from the user data, so input can be treated as only data
+    const query = `
+      INSERT INTO trades (trade_date, asset, direction, outcome, net_pnl)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `; // return the new trade back to client
+
+    const tradeValues = [trade_date, asset, direction, outcome, net_pnl];
+    const newTrade = await pool.query(query, tradeValues); // Execute the query with the values provided
+
+    // 201: successfully created
+    res.status(201).json(newTrade.rows[0]); // Send the first row to client that represents the new trade
+
+  } catch (err) {
+    console.error('Error inserting a new trade into database', err.message);
+    res.status(500).json({ error: 'Error inserting a new trade into database' });
   }
 });
 
