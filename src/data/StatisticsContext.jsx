230f669fbfdfeb663ea1bf0b7010b5adfe350f89
Haveadream1 <260602david@gmail.com>
2026-04-26 08:25:53 +0900

    feat: wrap fetch function in a useEffect

--- src/data/StatisticsContext.jsx
+++ src/data/StatisticsContext.jsx
@@ -1,28 +1,31 @@
 // Data context for statistics (both dashboard and analytics page)
 // Avoid having to fetch data from the road on 2 different pages
 
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const DataContext = createContext(null);
 
-export function DataProvider({ children }) {
-    const [stats, setStats] = useState([]);
+export function StatisticsProvider({ children }) {
+    const [stats, setStats] = useState({}); // setting up an empty object leave it to be undefined until we fetch data
 
-    const fetchStatistics = async () => {
-        try {
-            const response = await fetch('/api/statistics');
-            const data = await response.json();
+    useEffect(() => {
+        const fetchStatistics = async () => {
+            try {
+                const response = await fetch('/api/statistics');
+                const data = await response.json();
 
-            if (response.ok) {
-                console.log('Successfully fetched statistics data');
-                setStats(data);
-            } else {
-                console.error('Failed to fetch statistics data', data.error);
+                if (response.ok) {
+                    console.log('Successfully fetched statistics data');
+                    setStats(data);
+                } else {
+                    console.error('Failed to fetch statistics data', data.error);
+                }
+            } catch (err) {
+                console.error('Error fetching analytics from database', err.message);
             }
-        } catch (err) {
-            console.error('Error fetching analytics from database', err.message);
         }
-    }
+        fetchStatistics();
+    }, [])
 
     return (
         <DataContext.Provider value={stats}>
@@ -31,10 +34,10 @@ export function DataProvider({ children }) {
     );
 }
 
-export const useData = () =>  {
+export const useStatistics = () =>  {
     const context = useContext(DataContext);
     if (!context) {
-        throw new Error('useData must be inside a DataProvider');
+        throw new Error('useStatistics must be inside a StatisticsProvider');
     }
     return context;
 }
