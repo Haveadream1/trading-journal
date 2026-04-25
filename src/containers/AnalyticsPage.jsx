c0ecba338d9018366b9eb3c8a5f914c4505d111b
Haveadream1 <260602david@gmail.com>
2026-04-26 08:26:16 +0900

    feat: display statistics data

--- src/containers/AnalyticsPage.jsx
+++ src/containers/AnalyticsPage.jsx
@@ -1,9 +1,12 @@
 import AnalyticSection from "../components/AnalyticSection";
 import Header from "../components/Header";
 import MainHeading from "../components/MainHeading";
-
+import { useStatistics } from "../data/StatisticsContext";
 
 export default function AnalyticsPage() {
+    const stats = useStatistics();
+
+    // TODO: if we keep total number of trades, then need to check in the code if we need to replace account-grow
     return (
         <>
             <Header />
@@ -25,21 +28,29 @@ export default function AnalyticsPage() {
                             titleId="weekly-results-heading"
                             titleValue="Weekly results"
                             text="+856,40 €"
+                            text={`+ ${stats.weeklyPnl} €`}
                         />
 
                         <AnalyticSection
                             titleId="biggest-win-heading"
                             titleValue="Biggest win"
-                            text="250,24 €"
+                            // text="250,24 €"
+                            text={`${stats.biggestWin} €`}
                         />
                     </div>
 
                     <div className="analytics-sub-right-grid">
-                        <AnalyticSection
+                        {/* <AnalyticSection
                             titleId="account-grow-heading"
                             titleValue="Monthly account grow"
                             text="+32%"
+                        /> */}
+                        <AnalyticSection
+                            titleId="total-number-trades-heading"
+                            titleValue="Total number of trades"
+                            text={`${stats.totalTrades}`}
                         />
+
 
                         <AnalyticSection
                             titleId="average-graph-heading"
