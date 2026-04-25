70773e1acb46b720318d99f1188f941ba6040629
Haveadream1 <260602david@gmail.com>
2026-04-25 22:25:25 +0900

    feat: display data from statistics data context

--- src/containers/DashboardPage.jsx
+++ src/containers/DashboardPage.jsx
@@ -3,9 +3,16 @@ import MetricSection from "../components/MetricSection";
 import Header from "../components/Header";
 import SideMetricSection from "../components/SideMetricSection";
 import { useData } from "../data/DataContext";
+import { useStatistics } from '../data/StatisticsContext'
+import { useEffect } from "react";
 
 export default function DashboardPage() {
     const articles = useData();
+    const stats = useStatistics();
+
+    useEffect(() => {
+        console.log(stats);
+    }, [])
 
     return (
         <>
@@ -20,19 +27,22 @@ export default function DashboardPage() {
                     <MetricSection 
                         titleId="metric-heading-total-pnl" 
                         titleValue="Total P/L" 
-                        text="15,150.25 €" 
+                        // text="15,150.25 €" 
+                        text={`${stats.totalPnl} €`}
                     />
 
                     <MetricSection 
                         titleId="metric-heading-win-rate" 
                         titleValue="Win Rate" 
-                        text="75.2 %" 
+                        // text="75.2 %" 
+                        text={`${stats.winRate} %`}
                     />
 
                     <MetricSection 
                         titleId="metric-heading-profit-factor" 
                         titleValue="Profit factor" 
-                        text="2.12" 
+                        // text="2.12" 
+                        text={`${stats.profitFactor}`}
                     />
                 </div>
 
