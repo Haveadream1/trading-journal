0a86c1111a457cfbbc474b76ff3183ae0318b8fa
Haveadream1 <260602david@gmail.com>
2026-04-26 23:02:56 +0900

    refactor: to accept the trades by props

--- src/components/SideMetricSection.jsx
+++ src/components/SideMetricSection.jsx
@@ -4,31 +4,33 @@ export default function SideMetricSection({
     titleId,
     titleValue,
     graphMetric,
+    trades,
     assetMetric,
     articleMetric,
     data,
     textAsset,
     textCount
 }) {
-    const trades = [
-        { id: 1, asset: 'AAPL', direction: 'Buy', outcome: 'Win', date: '2024-01-15', netPNL: 200},
-        { id: 2, asset: 'NVDA', direction: 'Sell', outcome: 'Loss', date: '2024-01-16', netPNL: -100},
-        { id: 3, asset: 'AUD/USD', direction: 'Buy', outcome: 'Win', date: '2024-01-17', netPNL: 150},
-        { id: 4, asset: 'ETH', direction: 'Sell', outcome: 'Win', date: '2024-01-18', netPNL: 350},
-        { id: 5, asset: 'BTC', direction: 'Buy', outcome: 'Loss', date: '2024-01-19', netPNL: -200},
-        { id: 6, asset: 'USD coin', direction: 'Sell', outcome: 'Loss', date: '2024-01-20', netPNL: -150},
-    ];
+    // const trades = [
+    //     { id: 1, asset: 'AAPL', direction: 'Buy', outcome: 'Win', date: '2024-01-15', netPNL: 200},
+    //     { id: 2, asset: 'NVDA', direction: 'Sell', outcome: 'Loss', date: '2024-01-16', netPNL: -100},
+    //     { id: 3, asset: 'AUD/USD', direction: 'Buy', outcome: 'Win', date: '2024-01-17', netPNL: 150},
+    //     { id: 4, asset: 'ETH', direction: 'Sell', outcome: 'Win', date: '2024-01-18', netPNL: 350},
+    //     { id: 5, asset: 'BTC', direction: 'Buy', outcome: 'Loss', date: '2024-01-19', netPNL: -200},
+    //     { id: 6, asset: 'USD coin', direction: 'Sell', outcome: 'Loss', date: '2024-01-20', netPNL: -150},
+    // ];
 
+    // ! 2 issues data is not ordered, negative values are not represented
     return (
         <section className={graphMetric ? ("trade-results-graphic") : ("side-metric-section")} aria-labelledby={titleId}>
             <h3 id={titleId}>{titleValue}</h3>
             {graphMetric && (
                 <ResponsiveContainer> {/* Allow to fix issue of page resizing with this recharts import */}
                     <AreaChart responsive data={trades}>
-                        <XAxis dataKey="date" />
-                        <YAxis width="auto" type="number" />
+                        <XAxis dataKey='trade_date' />
+                        <YAxis width="auto" type="net_pnl" />
                         <Tooltip />
-                        <Area type="monotone" dataKey="netPNL" stroke="#11D473" fill="#11D473" />
+                        <Area type="monotone" dataKey="net_pnl" stroke="#11D473" fill="#11D473" />
                     </AreaChart>
                 </ResponsiveContainer>
             )}
