f8b5df2a80a179545df62e8d6291f10b4a065f7b
Haveadream1 <260602david@gmail.com>
2026-04-29 11:42:56 +0900

    feat: add comments to facilitate the reading

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -5,13 +5,14 @@ export default function TableRow({
     outcome,
     pnl
 }) {
-    // TODO add a button to remove the trade from the journal
-    
+    // ! Check later: Loss and loss
     return (
         <tr className="table-values-row">
             <td className="table-date-value">{date}</td>
             <td className="table-asset-value">{asset}</td>
             <td className="table-direction-value">{direction}</td>
+            
+            {/* conditionaly render depending on the outcome of the trade */}
             <td className="table-outcome-value">
                 {outcome === "loss" ? (
                     <span className="outcome-span-loss">Loss</span>
