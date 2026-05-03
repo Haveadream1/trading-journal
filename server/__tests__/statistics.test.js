eba8c49324b555d327e83109224b53db611716a4
Haveadream1 <260602david@gmail.com>
2026-05-04 00:03:16 +0900

    feat: add testing for statistics route

---
+++ server/__tests__/statistics.test.js
@@ -0,0 +1,23 @@
+// Test if we don't get errors when database is empty
+
+const request = require('supertest');
+const { describe } = require("jest-circus");
+const app = require('../app');
+const pool = require('../database');
+
+jest.mock('../database', () => ({
+    query: jest.fn()
+}))
+
+describe('GET route /api/statistics', () => {
+    it('return totalTrades equal to 0 when database is empty', async () => {
+        // Simulate totalResult.rows[0].total
+        pool.query.mockResolvedValueOnce({ 
+            rows: [{ total: 0}]
+        });
+
+        const response = await request(app).get('/api/statistics');
+
+        expect(response.body.totalTrades).toBe(0);
+    })
+})
