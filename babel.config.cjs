90e00ed994eb716fee735faba2f9c5421c7d6c1a
Haveadream1 <260602david@gmail.com>
2026-05-04 14:53:11 +0900

    refactor: comments

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',      // Transforms modern JavaScript
-    '@babel/preset-react'     // Transforms JSX
+    '@babel/preset-env', // allow us to use the ES6 features
+    '@babel/preset-react' // allow to transform React code into JS
   ]
 };
