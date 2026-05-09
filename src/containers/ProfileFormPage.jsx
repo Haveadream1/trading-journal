a29da08d47fccf50e907cb616824f985ff97cd72
Haveadream1 <260602david@gmail.com>
2026-05-09 23:48:52 +0900

    feat: handle profile form submit

--- src/containers/ProfileFormPage.jsx
+++ src/containers/ProfileFormPage.jsx
@@ -1,12 +1,47 @@
 /* Style import */
+    // Share the same style, as we want it to be identical to form page
 import '../styles/ProfileStyle.css'
 
-// Share the same style, as we want it to be identical to form page
-
-import { Link } from "react-router";
+import { Link, Navigate, useNavigate } from "react-router";
 import MainHeading from "../components/MainHeading";
+import { useState } from 'react';
+import { useProfile } from '../data/ProfileContext';
 
 export default function ProfileFormPage() {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const {handleDataChange, storeProfile, profileData} = useProfile()
+    const navigate = useNavigate();
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        const label = e.target.id;
+
+        handleDataChange(label, value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (isSubmitting) return;
+        setIsSubmitting(true);
+
+        try {
+            // Function called return a boolean value depending on success or failure
+            const success = storeProfile(profileData);
+
+            if (success) {
+                console.log("Successes in submitting the profile form");
+                navigate('/profile');
+            } else {
+                console.error("Error while submitting the profile form");
+                setIsSubmitting(false);
+            }
+        } catch(error) {
+            console.error("Failed to submit profile form", error);
+            setIsSubmitting(false);
+        }
+    }
+
     return (
         <>
             <header className="profile-header">
@@ -20,38 +55,31 @@ export default function ProfileFormPage() {
                 <div className="profile-section">
                     <div className="profile-picture-container">
                         <div className="picture-container">
-                            <img src="" id="profile-picture" />
+                            {/* <img src="" id="profile-picture" /> */}
                             <p>User name</p>
                         </div>
 
                         <div className="button-container">
-                            <Link to={"/profile"} className='profile-save-button' aria-label='save input and redirect to profile page'>Save</Link>
+                            {/* form link the button to the form, as it's located outside */}
+                            <button type='submit' form="profile-form" disabled={isSubmitting} className='profile-save-button' >
+                                {isSubmitting ? "Saving..." : "Save"}
+                            </button>
                             <Link to={"/profile"} className="profile-cancel-button" aria-label='redirect to profile page'>Cancel</Link>
                         </div>
                     </div>
 
-                    {/* <div className="profile-data-container">
-                        <div className="data-child-container">
-                            <p>Name:</p>
-                            <p className="user-name-value">User name</p>
-                        </div>
-                        <div className="data-child-container">
-                            <p>Motivation:</p>
-                            <p className="user-motivation-value">My motivation is...</p>
-                        </div>
-                    </div> */}
-
                     {/* ! Button should be inside for the submit ? */}
-                    <form action="post" className="profile-form">
-                        <fieldset id="profile-name-fieldset" className="profile-fieldset">
-                            <label htmlFor="user-name-value">Name:</label>
-                            <input type="text" id="user-name-value" className="profile-input" placeholder="My name is...."></input>
-                        </fieldset>
+                    {/* No fieldset because we don't implement a legend */}
+                    <form onSubmit={handleSubmit} id="profile-form" className="profile-form">
+                        <div id="profile-name-fieldset" className="profile-fieldset">
+                            <label htmlFor="username-input">Name:</label>
+                            <input type="text" onChange={handleInputChange} id="username-input" className="profile-input" maxLength={15} placeholder="My name is...." />
+                        </div>
 
-                        <fieldset id="profile-motivation-fieldset" className="profile-fieldset">
-                            <label htmlFor="motivation-value">Motivation:</label>
-                            <input type="text" id="motivation-value" className="profile-input" placeholder="My motivation is...."></input>
-                        </fieldset>
+                        <div id="profile-motivation-fieldset" className="profile-fieldset">
+                            <label htmlFor="motivation-input">Motivation:</label>
+                            <input type="text" onChange={handleInputChange} id="motivation-input" className="profile-input" maxLength={25} placeholder="My motivation is...." />
+                        </div>
                     </form>
                 </div>
             </main>
