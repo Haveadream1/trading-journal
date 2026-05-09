3472b8f415b257df592954693decde3afd483072
Haveadream1 <260602david@gmail.com>
2026-05-09 23:48:24 +0900

    feat: use context to store and get profile

--- src/data/ProfileContext.jsx
+++ src/data/ProfileContext.jsx
@@ -1,11 +1,53 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const FormContext = createContext(null);
 
 export default function ProfileProvider({ children}) {
+    const [profileData, setProfileData] = useState({
+        "username-input": "Anonymous",
+        "motivation-input":  ""
+    });
+
+    // Get the profile from local storage for inital mount
+    useEffect(() => {
+        const getProfile = async () => {
+            try {
+                const storedProfile = JSON.parse(localStorage.getItem('profile'));
+                if (!storedProfile) return
+
+                setProfileData(storedProfile);
+            } catch(error) {
+                console.error('Failed to retrieve profile from local storage', error);
+            }
+        }
+        getProfile();
+    }, []);
+
+    const handleDataChange = (label, value) => {
+        setProfileData((prev) => ({
+            ...prev,
+            [label]: value
+        }))
+    }
+
+    const storeProfile = (profileData) => {
+        // Save profile data into local storage: key, value
+        try {
+            const storedProfile = localStorage.setItem('profile', JSON.stringify(profileData));
+            if (!storedProfile) return false;
+
+            // Return a boolean used in the profile form for validation on submit
+            return true;
+        } catch(error) {
+            console.log('Failed to store the profile in local storage', error);
+            return false
+        }
+    }
 
     const value = {
-
+        profileData,
+        handleDataChange,
+        storeProfile
     };
 
     return (
@@ -15,10 +57,10 @@ export default function ProfileProvider({ children}) {
     )
 }
 
-export const useForm = () => {
+export const useProfile = () => {
     const context = useContext(FormContext);
     if (!context) {
-        throw new Error("useForm must be in a FormProvider");
+        throw new Error("useProfile must be in a FormProvider");
     }
     return context;
 }
