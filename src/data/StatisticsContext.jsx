6f7efe91dde96d9a4d9d04980b58a59f0df0f854
Haveadream1 <260602david@gmail.com>
2026-05-10 16:31:58 +0900

    feat: export refresh trigger

--- src/data/StatisticsContext.jsx
+++ src/data/StatisticsContext.jsx
@@ -72,6 +72,7 @@ export function StatisticsProvider({ children }) {
         analyticSectionColor,
         sideMetricColor,
         refreshStatistics,
+        refreshTrigger,
         isLoading
     }
 
