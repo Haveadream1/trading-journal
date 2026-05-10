5f1241aa5b68f5218261eac5e6d2ff54158cb1c6
Haveadream1 <260602david@gmail.com>
2026-05-10 16:30:54 +0900

    feat: create async function to delete trade via route

--- src/data/ActionsContext.jsx
+++ src/data/ActionsContext.jsx
@@ -3,13 +3,34 @@ import { createContext, useContext } from "react";
 const ActionsContext = createContext(null);
 
 export function ActionsProvider({ children }) {
+    const deleteTrade = async (id) => {
+        try {
+            // Pass the id in the route to query the correct trade
+            const response = await fetch(`/api/trades/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
 
-    const values = {
+            const data = await response.json();
+            
+            if (!response.ok) {
+                console.error("Failed to delete the trade", data.error);
+            }
 
+            return data;
+        } catch(error) {
+            console.error("Error deleting the trade by id", error.message);
+        }
+    }
+
+    const value = {
+        deleteTrade
     };
 
     return (
-        <ActionsContext.Provider values={values}>
+        <ActionsContext.Provider value={value}>
             {children}
         </ActionsContext.Provider>
     );
