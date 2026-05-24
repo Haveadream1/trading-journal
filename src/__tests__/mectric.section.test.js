b857d0bde65e2d69488aa0774cfebe0fce781fa7
Haveadream1 <260602david@gmail.com>
2026-05-24 10:21:04 +0900

    feat: mock context

--- src/__tests__/mectric.section.test.js
+++ src/__tests__/mectric.section.test.js
@@ -3,12 +3,24 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, jest } from "@jest/globals";
 import { render, screen } from '@testing-library/react';
 import MetricSection from '../components/MetricSection';
+import { useStatistics } from '../data/StatisticsContext';
+
+// Mock statistics context
+jest.mock('../data/statisticsContext', () => ({
+    useStatistics: jest.fn()
+}))
 
 describe('GET metric component', () => {
     it('displays the title and value of the metric', () => {
+
+        // Mock the value fetched from the hook
+        useStatistics.mockReturnValue({
+            isLoading: false
+        });
+
         render(
             <MetricSection
                 titleId='metric-heading-total-pnl'
