db87156ec0f7e0c665f25b12b8b931c5697d09e8
Haveadream1 <260602david@gmail.com>
2026-05-24 16:01:48 +0900

    feat: add test for DELETE route

--- server/__tests__/delete.trades.test.js
+++ server/__tests__/delete.trades.test.js
@@ -15,18 +15,38 @@ describe('DELETE route /api/trades/:id', () => {
     })
 
     it('return an error status for an NAN id', async () => {
-
+        const response = await request(app).delete('/api/trades/qqq');
+            
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('Invalid id for deleting trade');    
     })
 
     it('return an error status for a trade not found', async () => {
+        pool.query.mockResolvedValueOnce({ rows:[] });
 
+        const response = await request(app).delete('/api/trades/9');
+        
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe('Trade not found in database');
     })
 
     it('return a success status for a trade deleted', async () => {
-
+        pool.query.mockResolvedValueOnce({
+            rows: [{ id: 1, trade_date: '2025/01/01', asset: 'EUR/USD', direction: 'win', outcome: 'win', net_pnl: 40 }]
+        })
+        
+        const response = await request(app).delete('/api/trades/1');
+    
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Trade deleted successfully by id');    
     })
 
     it('return an error status for a failed attempt to delete the trade', async () => {
-        
+        pool.query.mockRejectedValueOnce(new Error('Connection failed'));
+
+        const response = await request(app).delete('/api/trades/1');
+
+        expect(response.status).toBe(500);
+        expect(response.body.error).toBe('Error deleting trade with id');
     })
 })
