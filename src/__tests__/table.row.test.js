b0b8f8df46a25714a2325b7310f2f905e9b27ef1
Haveadream1 <260602david@gmail.com>
2026-05-24 12:07:17 +0900

    fix: testing by adding mocking

--- src/__tests__/table.row.test.js
+++ src/__tests__/table.row.test.js
@@ -1,22 +1,45 @@
 // Unit testing table row component
 
+// Mock before the imports 
+jest.mock('../assets/EditTradeIcon.svg', () => 'edit-icon-mock');
+jest.mock('../assets/DeleteTradeIcon.svg', () => 'delete-icon-mock');
+
+// Mock the custom hook and the functions it returns
+jest.mock('../hooks/useTradeActions', () => ({
+    useTradeActions: jest.fn(() => ({
+        handleEditClick: jest.fn(),
+        handleDeleteClick: jest.fn()
+    }))
+}));
+
+// Refers to documentation on mock Link of router
+jest.mock('react-router', () => ({
+    Link: ({ children, ...props }) => <a {...props}>{children}</a>
+}))
+
 import React from 'react';
 import '@testing-library/jest-dom';
 
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, jest} from "@jest/globals";
 import { render, screen } from '@testing-library/react';
 import TableRow from '../components/TableRow';
+import { Link, MemoryRouter } from 'react-router';
 
-describe('GET table row component', () => {
+describe('GET table row component', () => { 
     it('displays correctly a winning trade in the table', () => {
         render(
-            <TableRow 
-                date='2026/03/30'
-                asset='NVDA'
-                direction='buy'
-                outcome='win'
-                pnl={95}
-            />
+            <table>
+                <tbody>
+                    <TableRow 
+                        date='2026/03/30'
+                        asset='NVDA'
+                        direction='buy'
+                        outcome='win'
+                        pnl={95}
+                        onTradeDeleted={jest.fn()}
+                    />
+                </tbody>
+            </table>
         )
 
         expect(screen.getByText('NVDA')).toBeInTheDocument();
@@ -25,13 +48,18 @@ describe('GET table row component', () => {
 
     it('displays correctly a loosing trade in the table', () => {
         render (
-            <TableRow 
-                date='2026/04/12'
-                asset='AMZN'
-                direction='sell'
-                outcome='loss'
-                pnl={-120}
-            />
+            <table>
+                <tbody>
+                    <TableRow 
+                        date='2026/04/12'
+                        asset='AMZN'
+                        direction='sell'
+                        outcome='loss'
+                        pnl={-120}
+                        onTradeDeleted={jest.fn()}
+                    />
+                </tbody>
+            </table>
         )
 
         expect(screen.getByText('Loss')).toBeInTheDocument();
